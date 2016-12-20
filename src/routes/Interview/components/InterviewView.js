edec83319366e364595923e5d3a8ba86da75f735
Lucas Borella <blackbox.software@gmail.com>
2016-12-20 20:25:18 -0300

    Initial Commit

---
+++ src/routes/Interview/components/InterviewView.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './InterviewView.scss'
+
+export const InterviewView = () => (
+  <div>
+    <h4>Interview!!!</h4>
+
+  </div>
+)
+
+export default InterviewView
---
+++ src/routes/Interview/index.js
@@ -0,0 +1,6 @@
+import InterviewView from './components/InterviewView'
+
+// Sync route definition
+export default {
+  component : InterviewView
+}
